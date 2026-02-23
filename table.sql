0729816d52afe97b95866092c0ce099134447a80
victor170307 <snxwewa@gmail.com>
2026-02-23 14:40:34 +0100

    creation des tables ( vehicule, station, client, location )

--- table.sql
+++ table.sql
@@ -1,4 +1,3 @@
--- Création de la table vehicule
 CREATE TABLE vehicule (
     id_vehicule SERIAL PRIMARY KEY,
     marque VARCHAR(100) NOT NULL,
@@ -9,14 +8,12 @@ CREATE TABLE vehicule (
     ville VARCHAR(100) NOT NULL
 );
 
--- Création de la table station
 CREATE TABLE station (
     id_station SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     ville VARCHAR(100) NOT NULL
 );
 
--- Création de la table client
 CREATE TABLE client (
     id_client SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
@@ -24,15 +21,14 @@ CREATE TABLE client (
     email VARCHAR(150) UNIQUE NOT NULL
 );
 
--- Création de la table location
 CREATE TABLE location (
     id_location SERIAL PRIMARY KEY,
     date_debut TIMESTAMP NOT NULL,
-    date_fin TIMESTAMP, -- Peut être NULL si la location est en cours
+    date_fin TIMESTAMP, 
     id_client INT NOT NULL,
     id_vehicule INT NOT NULL,
     id_station_depart INT NOT NULL,
-    id_station_arrivee INT, -- Peut être NULL si le véhicule n'est pas encore rendu
+    id_station_arrivee INT, 
 
     CONSTRAINT fk_client FOREIGN KEY (id_client) REFERENCES client(id_client),
     CONSTRAINT fk_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule),
